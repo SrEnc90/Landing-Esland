94e74b40d2becdefd7b9f775444f9e12f43f616c
SrEnc90 <carl.enc90@gmail.com>
2024-04-19 06:55:22 -0500

    arreglar las imagenes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,12 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'ca'],
+    routing: {
+      prefixDefaultLocale: false // Significa que cuando ingresen a /es solamente debemos colocar el / (es el idioma por defecto)y para catalán colocamos el /ca
+    }
+  }
 });
